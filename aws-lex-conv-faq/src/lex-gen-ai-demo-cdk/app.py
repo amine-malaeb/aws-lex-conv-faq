3012d587aa89c559c57cf093dd28c0d6d533be85
Amine Malaeb <146562782+amine-malaeb@users.noreply.github.com>
2023-12-20 20:23:11 +0200

    Update app.py

--- aws-lex-conv-faq/src/lex-gen-ai-demo-cdk/app.py
+++ aws-lex-conv-faq/src/lex-gen-ai-demo-cdk/app.py
@@ -6,7 +6,7 @@ from endpoint_handler import create_endpoint_from_HF_image
 
 # create_endpoint_from_HF_image(hf_model_id, instance_type="ml.g5.8xlarge", endpoint_name=SAGEMAKER_ENDPOINT_NAME, number_of_gpu=1)
 # You can run with no arguments to get default values of google/flan-t5-xxl on ml.g5.8xlarge, or pass in your own arguments
-create_endpoint_from_HF_image(hf_model_id="tiiuae/falcon-7b-instruct")
+#create_endpoint_from_HF_image(hf_model_id="tiiuae/falcon-7b-instruct")
 
 app = cdk.App()
 filestack = LexGenAIDemoFilesStack(app, "LexGenAIDemoFilesStack")
